45eb75886443b6df3b18ea70d417f5bb8626bf6d
haipengl <haipengl@sfu.ca>
2018-10-29 21:41:30 -0700

    add more logs

--- test_models.py
+++ test_models.py
@@ -21,9 +21,15 @@ if __name__ == '__main__':
     test_accuracy_list = []
 
     for model_name in model_list:
+        print("testing on model: {}".format(model_name))
         model.load_state_dict(torch.load(model_name))
-        train_loss, train_accuracy = interence.inference_on_train(model, cuda=False)
-        test_loss, test_accuracy = interence.inference_on_test(model, cuda=False)
+        train_loss, train_accuracy = interence.inference_on_train(model, cuda=True)
+        print("\ttrain_loss: {}".format(train_loss))
+        print("\ttrain_accuracy: {}".format(train_accuracy))
+        test_loss, test_accuracy = interence.inference_on_test(model, cuda=True)
+        print("\ttest_loss: {}".format(test_loss))
+        print("\ttest_accuracy: {}".format(test_accuracy))
+
         train_loss_list.append(train_loss)
         train_accuracy_list.append(train_accuracy)
         test_loss_list.append(test_loss)
